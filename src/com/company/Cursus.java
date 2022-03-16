14c285e77dac7a3646df1b5e1a1e80d11aa75c54
nathan79c <19kt210@esisalama.org>
2022-03-16 05:11:45 +0100

    fin de la classe Cursus

--- src/com/company/Cursus.java
+++ src/com/company/Cursus.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Cursus {
-}
+    private Etudiant [] Liste;
+    public Cursus(){
+        var lectureClavier = new Scanner(System.in);
+        System.out.println("Nombres D'Etudiants : ");
+        var nbEtudiants = lectureClavier.nextInt();
+        Liste = new Etudiant [nbEtudiants];
+        for (var i = 0; i < Liste.length ; i++){
+            Liste[i] = new Etudiant();
+        }
+    }
+    public void afficheEtudiant(){
+        for (var e : Liste) e.afficheEtudiant();
+    }
+}// Fin de la classe Cursus;
